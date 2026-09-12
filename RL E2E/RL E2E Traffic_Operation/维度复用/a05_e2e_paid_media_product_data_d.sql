eea10a276a99c246bbc4101f83c0091c4ef23fa2
辜涛 <1370391119@qq.com>
2026-09-12 15:46:57 +0800

    RL E2E 测试-RL E2E Traffic_Operation Category Growth - 23

--- RL E2E/RL E2E Traffic_Operation/维度复用/a05_e2e_paid_media_product_data_d.sql
+++ RL E2E/RL E2E Traffic_Operation/维度复用/a05_e2e_paid_media_product_data_d.sql
@@ -1,10 +1,11 @@
 let
     源 = Odbc.Query(
-        "dsn=bytehouse_rl", 
+        "
+        dsn=bytehouse_rl", 
         "
 select  *,
-IF(framework IS NOT NULL AND brand IS NOT NULL AND category IS NOT NULL AND channel IS NOT NULL AND mix_msg IS NULL,'ALL',mix_msg) AS mix_msg_Label_01,
-IF(season IS NOT NULL AND brand IS NOT NULL AND category IS NOT NULL AND channel IS NOT NULL AND mix_msg IS NULL,'ALL',mix_msg) AS mix_msg_Label_02
+IF(framework IS NOT NULL AND brand IS NOT NULL AND category IS NOT NULL AND channel IS NOT NULL AND mix_msg IS NULL,'other',mix_msg) AS mix_msg_Label_01,
+IF(season IS NOT NULL AND brand IS NOT NULL AND category IS NOT NULL AND channel IS NOT NULL AND mix_msg IS NULL,'other',mix_msg) AS mix_msg_Label_02
   from  `indep_rl_ads`.a05_e2e_paid_media_product_data_d 
     WHERE platform IN ('JD', 'TM') 
         "
